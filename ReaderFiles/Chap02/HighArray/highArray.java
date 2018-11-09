f68bef1d5f2d6dd27d9070b64cc23902c161c842
madjo <madjo85@yandex.ru>
2018-11-09 18:38:47 +0300

    first commit

---
+++ ReaderFiles/Chap02/HighArray/highArray.java
@@ -0,0 +1,123 @@
+// highArray.java
+// demonstrates array class with high-level interface
+// to run this program: C>java HighArrayApp
+////////////////////////////////////////////////////////////////
+class HighArray
+   {
+   private long[] a;                 // ref to array a
+   private int nElems;               // number of data items
+   //-----------------------------------------------------------
+   public HighArray(int max)         // constructor
+      {
+      a = new long[max];                 // create the array
+      nElems = 0;                        // no items yet
+      }
+   //-----------------------------------------------------------
+   public boolean find(long searchKey)
+      {                              // find specified value
+      int j;
+      for(j=0; j<nElems; j++)            // for each element,
+         if(a[j] == searchKey)           // found item?
+            break;                       // exit loop before end
+      if(j == nElems)                    // gone to end?
+         return false;                   // yes, can't find it
+      else
+         return true;                    // no, found it
+      }  // end find()
+   //-----------------------------------------------------------
+   public void insert(long value)    // put element into array
+      {
+      a[nElems] = value;             // insert it
+      nElems++;                      // increment size
+      }
+   //-----------------------------------------------------------
+   public boolean delete(long value)
+      {
+      int j;
+      for(j=0; j<nElems; j++)        // look for it
+         if( value == a[j] )
+            break;
+      if(j==nElems)                  // can't find it
+         return false;
+      else                           // found it
+         {
+         for(int k=j; k<nElems; k++) // move higher ones down
+            a[k] = a[k+1];
+         nElems--;                   // decrement size
+         return true;
+         }
+      }  // end delete()
+   //-----------------------------------------------------------
+   public void display()             // displays array contents
+      {
+      for(int j=0; j<nElems; j++)       // for each element,
+         System.out.print(a[j] + " ");  // display it
+      System.out.println("");
+      }
+   //-----------------------------------------------------------
+   public long removeMax()
+       {
+           long maxValue = 0;
+           if (nElems == 0) return -1;
+
+           for (long l : a) {
+               if (maxValue < l) maxValue = l;
+           }
+
+           delete(maxValue);
+       return maxValue;
+       }
+   }  // end class HighArray
+////////////////////////////////////////////////////////////////
+class HighArrayApp
+   {
+   public static void main(String[] args)
+      {
+      int maxSize = 100;            // array size
+      HighArray arr;                // reference to array
+      arr = new HighArray(maxSize); // create the array
+      long[] sortedArray = new long[100];
+
+      arr.insert(77);               // insert 10 items
+      arr.insert(99);
+      arr.insert(44);
+      arr.insert(55);
+      arr.insert(22);
+      arr.insert(88);
+      arr.insert(11);
+      arr.insert(00);
+      arr.insert(66);
+      arr.insert(33);
+
+      arr.display();                // display items
+
+      int searchKey = 35;           // search for item
+      if( arr.find(searchKey) )
+         System.out.println("Found " + searchKey);
+      else
+         System.out.println("Can't find " + searchKey);
+
+      arr.delete(00);               // delete 3 items
+      arr.delete(55);
+      arr.delete(99);
+
+      arr.display();                // display items again
+      System.out.println(arr.removeMax());
+      arr.display();
+
+      int index = 0;
+      while (true) {
+          long i = arr.removeMax();
+          System.out.println("return value = " + i);
+          if (i < 0) break;
+          sortedArray[index] = i;
+          index++;
+          System.out.println(index);
+      }
+      System.out.println("Sorted Array :" );
+          for (int i = 0; i < index; i++) {
+              System.out.println(sortedArray[i]);
+
+          }
+      }  // end main()
+   }  // end class HighArrayApp
---
+++ ReaderFiles/Chap02/OrderArray/src/main/java/orderedArray.java
@@ -0,0 +1,157 @@
+// orderedArray.java
+// demonstrates ordered array class
+// to run this program: C>java OrderedApp
+////////////////////////////////////////////////////////////////
+class OrdArray
+   {
+      private long[] a;                 // ref to array a
+      private int nElems;               // number of data items
+      private boolean isFind;
+   //-----------------------------------------------------------
+   public OrdArray(int max)          // constructor
+      {
+      a = new long[max];             // create array
+      nElems = 0;
+      }
+
+   //-----------------------------------------------------------
+   public int size()
+      { return nElems; }
+   //-----------------------------------------------------------
+
+
+   public long[] getArray() {
+       return a;
+   }
+
+   public boolean isFind(long searchKey) {
+       find(searchKey);
+       return isFind;
+   }
+
+   public OrdArray merge(OrdArray firstArr, OrdArray secArr) {
+       for (long i: firstArr.getArray()) {
+           if (!secArr.isFind(i)) {
+               secArr.insert(i);
+           }
+       }
+       return secArr;
+   }
+
+   public int find(long searchKey)
+      {
+      int lowerBound = 0;
+      int upperBound = nElems-1;
+      int curIn;
+      isFind = true;
+
+      while(true)
+         {
+            curIn = (lowerBound + upperBound ) / 2;
+            if(a[curIn]==searchKey)
+               return curIn;              // found it
+            else
+               if(lowerBound > upperBound) {
+                  isFind = false;
+                  return lowerBound;             // can't find it
+               }
+            else                          // divide range
+               {
+               if(a[curIn] < searchKey)
+                  lowerBound = curIn + 1; // it's in upper half
+               else
+                  upperBound = curIn - 1; // it's in lower half
+               }  // end else divide range
+         }  // end while
+      }  // end find()
+   //-----------------------------------------------------------
+   public void insert(long value)    // put element into array
+      {
+         int j = find(value);
+         for(int k=nElems; k>j; k--)    // move bigger ones up
+            a[k] = a[k-1];
+         a[j] = value;                  // insert it
+         nElems++;                      // increment size
+      }  // end insert()
+   //-----------------------------------------------------------
+   public boolean delete(long value)
+      {
+      int j = find(value);
+      if(!isFind)                  // can't find it
+         return false;
+      else                           // found it
+         {
+         for(int k=j; k<nElems; k++) // move bigger ones down
+            a[k] = a[k+1];
+         nElems--;                   //  decrement size
+         return true;
+         }
+      }  // end delete()
+   //-----------------------------------------------------------
+   public void display()             // displays array contents
+      {
+      for(int j=0; j<nElems; j++)       // for each element,
+         System.out.print(a[j] + " ");  // display it
+      System.out.println("");
+      }
+   //-----------------------------------------------------------
+   }  // end class OrdArray
+
+////////////////////////////////////////////////////////////////
+class OrderedApp
+   {
+   public static void main(String[] args)
+      {
+      int maxSize = 100;             // array size
+      OrdArray arr, arr2;                  // reference to array
+      arr = new OrdArray(maxSize);   // create the array
+
+      arr.insert(77);                // insert 10 items
+      arr.insert(99);
+      arr.insert(44);
+      arr.insert(55);
+      arr.insert(22);
+      arr.insert(88);
+      arr.insert(11);
+      arr.insert(00);
+      arr.insert(66);
+      arr.insert(33);
+
+         arr.display();                 // display items
+
+         arr.delete(00);                // delete 3 items
+         arr.delete(55);
+         arr.delete(99);
+
+          arr.display();                 // display items again
+
+         arr2 = new OrdArray(maxSize);   // create the array
+          arr2.insert(123);
+          arr2.insert(12);
+          arr2.insert(52);
+          arr2.insert(43);
+
+
+      int searchKey = 55;            // search for item
+      if( arr.find(searchKey) != arr.size() )
+         System.out.println("Found " + searchKey);
+      else
+         System.out.println("Can't find " + searchKey);
+
+
+
+
+
+      arr.insert(45);
+      arr.display();
+      arr.insert(10);
+      arr.display();
+      arr.delete(80);
+      arr.display();
+
+      //check mergearray
+      OrdArray arr3 = arr.merge(arr, arr2);
+      arr3.display();
+
+      }  // end main()
+   }  // end class OrderedApp
